be34465273fccb111ee3d3d1387e368f98e67fe8
alexdreyesz <alexdreyesz@gmail.com>
2025-04-26 03:55:07 -0400

    Added All Badges

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -13,74 +13,74 @@ export default function About() {
                 <p className="flex-col">I'm Alex D. Reyes, a computer science student at the University of Central Florida, Knight Hacks Treasurer, and Researcher at the U.N.A.R.Y lab. In my free time, I enjoy hiking, photography, working on cars, playing video games, and spending time with friends and family.</p>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit flex-col mt-10 text-2xl text-white">
                 <p>Languages:</p>
-                <div className="flex pt-2 gap-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {languages.map((language) => (
-                        <img className="h-12 animation-rotate-y" src={language.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={language.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>FrameWorks:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {frameworks.map((framework) => (
-                        <img className="h-12" src={framework.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={framework.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>Libraries:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {libraries.map((library) => (
-                        <img className="h-12" src={library.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={library.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>Development Tools:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {developmentTools.map((tool) => (
-                        <img className="h-12" src={tool.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={tool.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>Design Tools:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {designTools.map((tool) => (
-                        <img className="h-12" src={tool.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={tool.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>Databases:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {databases.map((database) => (
-                        <img className="h-12" src={database.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={database.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>AI & LLMs:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {AILLM.map((ai) => (
-                        <img className="h-12" src={ai.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={ai.iconPath}/>
                     ))}
                 </div>
             </div>
 
-            <div className="w-250 h-fit mt-5 text-2xl text-white">
+            <div className="w-250 h-fit mt-10 text-2xl text-white">
                 <p>Professional:</p>
-                <div className="flex pt-2">
+                <div className="flex flex-wrap flex-row pt-2 gap-2">
                     {professional.map((tool) => (
-                        <img className="h-12" src={tool.iconPath}/>
+                        <img className="h-14 animation-rotate-y mt-2" src={tool.iconPath}/>
                     ))}
                 </div>
             </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+/* Constants For About Home Section */
 export const words = [
     {text: "React", imgPath: "src/assets/icons/badges/frameworks/react.svg"},
     {text: "Tailwind", imgPath: "src/assets/icons/badges/frameworks/tailwindcss.svg"},
@@ -5,6 +6,8 @@ export const words = [
     {text: "GSAP", imgPath: "src/assets/icons/badges/frameworks/gsap1.png"},
 ];
 
+
+/* Constants For About Section */
 export const languages = [
     {text: "C", iconPath: "src/assets/icons/badges/languages/c.svg"},
     {text: "C++", iconPath: "src/assets/icons/badges/languages/cplusplus.svg"},
@@ -26,28 +29,90 @@ export const languages = [
 
 export const frameworks = [
     {text: "React", iconPath: "src/assets/icons/badges/frameworks/react.svg"},
-];
-
-export const developmentTools = [
-    {text: "VScode", iconPath: "src/assets/icons/badges/development-tools/vscode.svg"},
-];
-
-export const designTools = [
-    {text: "Figma", iconPath: "src/assets/icons/badges/design-tools/figma.svg"},
-];
-
-export const databases = [
-    {text: "MariaDB", iconPath: "src/assets/icons/badges/databases/mariadb.svg"},
+    {text: "Angular", iconPath: "src/assets/icons/badges/frameworks/angular.svg"},
+    {text: "Electron", iconPath: "src/assets/icons/badges/frameworks/electron.svg"},
+    {text: "Express", iconPath: "src/assets/icons/badges/frameworks/express.svg"},
+    {text: "FastApi", iconPath: "src/assets/icons/badges/frameworks/fastapi.svg"},
+    {text: "Flask", iconPath: "src/assets/icons/badges/frameworks/flask.svg"},
+    {text: "GSAP", iconPath: "src/assets/icons/badges/frameworks/gsap1.png"},
+    {text: "OpenGL", iconPath: "src/assets/icons/badges/frameworks/opengl.svg"},
+    {text: "TailwindCSS", iconPath: "src/assets/icons/badges/frameworks/tailwindcss.svg"},
+    {text: "ThreeJS", iconPath: "src/assets/icons/badges/frameworks/threejs.svg"},
+    {text: "Vue", iconPath: "src/assets/icons/badges/frameworks/vuejs.svg"},
 ];
 
 export const libraries = [
     {text: "Bootstrap", iconPath: "src/assets/icons/badges/libraries/bootstrap.svg"},
+    {text: "Anaconda", iconPath: "src/assets/icons/badges/libraries/anaconda.svg"},
+    {text: "Framermotion", iconPath: "src/assets/icons/badges/libraries/framermotion.svg"},
+    {text: "Framework7", iconPath: "src/assets/icons/badges/libraries/framework7.svg"},
+    {text: "JQuery", iconPath: "src/assets/icons/badges/libraries/jquery.svg"},
+    {text: "Matlab", iconPath: "src/assets/icons/badges/libraries/matlab.svg"},
+    {text: "Matplotlib", iconPath: "src/assets/icons/badges/libraries/matplotlib.svg"},
+    {text: "NPM", iconPath: "src/assets/icons/badges/libraries/npm.svg"},
+    {text: "Numpy", iconPath: "src/assets/icons/badges/libraries/numpy.svg"},
+    {text: "Pandas", iconPath: "src/assets/icons/badges/libraries/pandas.svg"},
+    {text: "Poetry", iconPath: "src/assets/icons/badges/libraries/poetry.svg"},
+    {text: "Pytorch", iconPath: "src/assets/icons/badges/libraries/pytorch.svg"},
+    {text: "React Bootstrap", iconPath: "src/assets/icons/badges/libraries/reactbootstrap.svg"},
+    {text: "React Navigation", iconPath: "src/assets/icons/badges/libraries/reactnavigation.svg"},
+    {text: "React Router", iconPath: "src/assets/icons/badges/libraries/reactrouter.svg"},
+];
+
+export const developmentTools = [
+    {text: "VScode", iconPath: "src/assets/icons/badges/development-tools/vscode.svg"},
+    {text: "Eclipse", iconPath: "src/assets/icons/badges/development-tools/eclipse.svg"},
+    {text: "Flutter", iconPath: "src/assets/icons/badges/development-tools/flutter.svg"},
+    {text: "Git", iconPath: "src/assets/icons/badges/development-tools/git.svg"},
+    {text: "Github", iconPath: "src/assets/icons/badges/development-tools/github.svg"},
+    {text: "Jypyter", iconPath: "src/assets/icons/badges/development-tools/jupyter.svg"},
+    {text: "Kubernetes", iconPath: "src/assets/icons/badges/development-tools/kubernetes.svg"},
+    {text: "Linux", iconPath: "src/assets/icons/badges/development-tools/linux.svg"},
+    {text: "Maven", iconPath: "src/assets/icons/badges/development-tools/maven.svg"},
+    {text: "Nodejs", iconPath: "src/assets/icons/badges/development-tools/nodejs.svg"},
+    {text: "Nodemon", iconPath: "src/assets/icons/badges/development-tools/nodemon.svg"},
+    {text: "Postman", iconPath: "src/assets/icons/badges/development-tools/postman.svg"},
+    {text: "Powershell", iconPath: "src/assets/icons/badges/development-tools/powershell.svg"},
+    {text: "Ubuntu", iconPath: "src/assets/icons/badges/development-tools/ubuntu.svg"},
+    {text: "Unity", iconPath: "src/assets/icons/badges/development-tools/unity.svg"},
+    {text: "Unreal Engine", iconPath: "src/assets/icons/badges/development-tools/unrealengine.svg"},
+    {text: "Visual Studio", iconPath: "src/assets/icons/badges/development-tools/visualstudio.svg"},
+    {text: "Vite", iconPath: "src/assets/icons/badges/development-tools/vite.svg"},
+    {text: "Vitejs", iconPath: "src/assets/icons/badges/development-tools/vitejs.svg"},
+];
+
+export const designTools = [
+    {text: "Figma", iconPath: "src/assets/icons/badges/design-tools/figma.svg"},
+    {text: "Blender", iconPath: "src/assets/icons/badges/design-tools/blender.svg"},
+    {text: "Canva", iconPath: "src/assets/icons/badges/design-tools/canva.svg"},
+    {text: "Latex", iconPath: "src/assets/icons/badges/design-tools/latex.svg"},
+    {text: "Photoshop", iconPath: "src/assets/icons/badges/design-tools/photoshop.svg"},
+    {text: "Wordpress", iconPath: "src/assets/icons/badges/design-tools/wordpress.svg"},
+];
+
+export const databases = [
+    {text: "MariaDB", iconPath: "src/assets/icons/badges/databases/mariadb.svg"},
+    {text: "Apache", iconPath: "src/assets/icons/badges/databases/apache.svg"},
+    {text: "Azure", iconPath: "src/assets/icons/badges/databases/azure.svg"},
+    {text: "Azure SQL", iconPath: "src/assets/icons/badges/databases/azuresqldatabase.svg"},
+    {text: "CosmosDB", iconPath: "src/assets/icons/badges/databases/cosmosdb.svg"},
+    {text: "mongoDB", iconPath: "src/assets/icons/badges/databases/mongodb.svg"},
+    {text: "Postgresql", iconPath: "src/assets/icons/badges/databases/postgresql.svg"},
 ];
 
 export const AILLM = [
     {text: "Chatgpt", iconPath: "src/assets/icons/badges/ai-llm/chatgpt.svg"},
+    {text: "Claude", iconPath: "src/assets/icons/badges/ai-llm/claude.svg"},
+    {text: "Copilot", iconPath: "src/assets/icons/badges/ai-llm/copilot.svg"},
+    {text: "Deepseek", iconPath: "src/assets/icons/badges/ai-llm/deepseek.svg"},
+    {text: "Google Gemini", iconPath: "src/assets/icons/badges/ai-llm/google-gemini.svg"},
+    {text: "Huggingface", iconPath: "src/assets/icons/badges/ai-llm/huggingface.svg"},
 ];
 
 export const professional = [
     {text: "Slack", iconPath: "src/assets/icons/badges/professional/slack.svg"},
+    {text: "Jira", iconPath: "src/assets/icons/badges/professional/jira.svg"},
+    {text: "LinkedIn", iconPath: "src/assets/icons/badges/professional/linkedin.svg"},
+    {text: "Notion", iconPath: "src/assets/icons/badges/professional/notion.svg"},
+    {text: "Stackoverflow", iconPath: "src/assets/icons/badges/professional/stackoverflow.svg"},
 ];
